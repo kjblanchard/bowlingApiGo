4aaf7e446b437754fb32506cf2ae321d157594d1
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-06-09 15:44:44 -0400

    Create other handlers

--- api/src/main.go
+++ api/src/main.go
@@ -1,155 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/kjblanchard/BowlingWebApp/handlers"
-	"github.com/kjblanchard/BowlingWebApp/models"
 )
 
 func main() {
 	// we will implement these handlers in the next sections
-	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/signin", handlers.Signin)
 	http.HandleFunc("/welcome", handlers.Welcome)
-	http.HandleFunc("/refresh", Refresh)
-	http.HandleFunc("/logout", Logout)
+	http.HandleFunc("/refresh", handlers.Refresh)
+	http.HandleFunc("/logout", handlers.Logout)
 
 	// start the server on port 8000
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-// Temporary users for testing
-var users = map[string]string{
-	"user1": "password1",
-	"user2": "password2",
-}
-
-// Create the JWT key used to create the signature
-var jwtKey = []byte("my_secret_key")
-
-// Create a struct to read the username and password from the request body
-type Credentials struct {
-	Password string `json:"password"`
-	Username string `json:"username"`
-}
-
-// Create the Signin handler
-func Signin(w http.ResponseWriter, r *http.Request) {
-	var creds Credentials
-	// Get the JSON body and decode into credentials
-	err := json.NewDecoder(r.Body).Decode(&creds)
-	if err != nil {
-		// If the structure of the body is wrong, return an HTTP error
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Get the expected password from our in memory map
-	expectedPassword, ok := users[creds.Username]
-
-	// If a password exists for the given user
-	// AND, if it is the same as the password we received, the we can move ahead
-	// if NOT, then we return an "Unauthorized" status
-	if !ok || expectedPassword != creds.Password {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	// Declare the expiration time of the token
-	// here, we have kept it as 5 minutes
-	expirationTime := time.Now().Add(5 * time.Minute)
-	// Create the JWT claims, which includes the username and expiry time
-	claims := &models.Claims{
-		Username: creds.Username,
-		RegisteredClaims: jwt.RegisteredClaims{
-			// In JWT, the expiry time is expressed as unix milliseconds
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
-		},
-	}
-
-	// Declare the token with the algorithm used for signing, and the claims
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	// Create the JWT string
-	tokenString, err := token.SignedString(jwtKey)
-	if err != nil {
-		// If there is an error in creating the JWT return an internal server error
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Finally, we set the client cookie for "token" as the JWT we just generated
-	// we also set an expiry time which is the same as the token itself
-	http.SetCookie(w, &http.Cookie{
-		Name:    "token",
-		Value:   tokenString,
-		Expires: expirationTime,
-	})
-}
-
-func Refresh(w http.ResponseWriter, r *http.Request) {
-	// (BEGIN) The code until this point is the same as the first part of the `Welcome` route
-	c, err := r.Cookie("token")
-	if err != nil {
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	tknStr := c.Value
-	claims := &models.Claims{}
-	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
-		return jwtKey, nil
-	})
-	if err != nil {
-		if err == jwt.ErrSignatureInvalid {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	if !tkn.Valid {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-	// (END) The code until this point is the same as the first part of the `Welcome` route
-
-	// We ensure that a new token is not issued until enough time has elapsed
-	// In this case, a new token will only be issued if the old token is within
-	// 30 seconds of expiry. Otherwise, return a bad request status
-	if time.Until(claims.ExpiresAt.Time) > 30*time.Second {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Now, create a new token for the current use, with a renewed expiration time
-	expirationTime := time.Now().Add(5 * time.Minute)
-	claims.ExpiresAt = jwt.NewNumericDate(expirationTime)
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtKey)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Set the new token as the users `token` cookie
-	http.SetCookie(w, &http.Cookie{
-		Name:    "token",
-		Value:   tokenString,
-		Expires: expirationTime,
-	})
-}
-
-func Logout(w http.ResponseWriter, r *http.Request) {
-	// immediately clear the token cookie
-	http.SetCookie(w, &http.Cookie{
-		Name:    "token",
-		Expires: time.Now(),
-	})
-}
+// var jwtKey = []byte("my_secret_key")
---
+++ api/src/models/users.go
@@ -0,0 +1,6 @@
+package models
+
+var Users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
